4b9247fdfe4c23d30a352a45c0edc14b33595fb1
Roofiif Ananta Fathan <roofiif2000018419@webmail.uad.ac.id>
2023-07-31 16:38:36 +0700

    beranda

--- Beranda/index.php
+++ Beranda/index.php
@@ -6,14 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style.css">
     <title>Document</title>
 </head>
 <body>
     <div>
         <nav class="nav-stl">
             <div class="navbar-left">
-                <img src="gambar/logo.png" alt="Logo Kiri">
+                <img src="../Gambar/logo.png" alt="Logo Kiri">
             </div>
 
             <div class="nav justify-content-center ">
@@ -25,19 +25,19 @@
             </div>
 
             <div class="navbar-right">
-                <img src="gambar/user.png" alt="Logo Kanan">
+                <img src="../Gambar/user.png" alt="Logo Kanan">
             </div>
         </nav>
     </div>
 
     <div class="page1"> 
         <!-- page 1 -->
-        <!-- <img src="gambar/homepagewoman.jpg" alt=""> -->
+        <!-- <img src="../Gambar/homepagewoman.jpg" alt=""> -->
 
         <div class="judul">
             <br>
             <p style="font-family: 'Montserrat';font-size: 42px; ">Our Service <br> We Deliver</p>
-            <hr class="yellow-hr">
+            <hr class="grs">
         </div>
     </div>
 
@@ -48,12 +48,12 @@
     </div>
 
     <div class="page2">
-        <img src="gambar/youngMan.png" class="youngMan">
+        <img src="../Gambar/youngMan.png" class="youngMan">
             <!-- <h3 class="tentangKami">Tentang Kami</h3> -->
 
         <div class="tentangKami">
             <h3>TENTANG KAMI</h3>
-            <HR>
+            <HR class="grs hr-page3">
             <P>
                 Berdiri sejak pada tahun 2021, PT. Luar Biasa Teknologi 
                 merupakan perusahaan Business Process Outsourcing 
@@ -69,8 +69,8 @@
     <div class="page3">
         <br>
         <h1 class="sertifikasi">SERTIFIKASI</h1>
-        <hr class="yellow-hr">
-        <img src="gambar/AFPI.png" class="afpi">
+        <hr class="grs hr-page4">
+        <img src="../Gambar/AFPI.png" class="afpi">
         
 
         <p>Terdaftar sebagai Anggota <br> Asosiasi Fintech Pendanaan Bersama Indonesia <br> Nomor 032/REG/DCO/202</p>
@@ -79,21 +79,36 @@
         <hr>
         <br>
         <h1>Diawasi</h1>
-        <hr class="yellow-hr">
-        <img src="gambar/OJK.png" class="ojk">
+        <hr class="grs hr-page4">
+        <img src="../Gambar/OJK.png" class="ojk">
         <br><br>
     </div>
 
     <div class="page4">
+        <hr>
+        <br>
         <h1>KLIEN KAMI</h1>
-        <hr class="yellow-hr">
-    </div>
+        <hr class="grs hr-page4"> 
+
+      <img src="../Gambar/klienKamiAbu.png" class="klien"> 
     
+    </div>
 
-    <script 
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-
-    </script>
+    <div id="halaman-kontak"></div>
 
 </body>
 </html>
+<script>
+        
+        function fileKontak() {
+            fetch('../kontak.php')
+            .then(response => response.text())
+            .then(data => {
+            document.getElementById('halaman-kontak').innerHTML = data;
+            });
+        }
+        window.onload = function() {
+            fileKontak();
+        };
+        // src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
