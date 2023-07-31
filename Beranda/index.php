d54d010ea627ef3b39f277631fe0acc1724a7d0e
Roofiif Ananta Fathan <roofiif2000018419@webmail.uad.ac.id>
2023-07-31 16:10:30 +0700

    tentang kami

---
+++ Beranda/index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Beranda</title>
+
+    <!-- Logo Title Bar-->
+
+    <link rel="icon" href="../Assets/img/lbt.png" type="image/x-icon">
+</head>
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-transparent w-100">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <img src="../Assets/img/lbt.png" alt="" width="50" height="50" class="d-inline-block align-text-top">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <ul class="navbar-nav mx-auto">
+            <li class="nav-item mx-3">
+              <a class="nav-link active" aria-current="page" href="">Beranda</a>
+            </li>
+            <li class="nav-item mx-3">
+              <a class="nav-link" href="../Tentang Kami">Tentang Kami</a>
+            </li>
+            <li class="nav-item mx-3">
+              <a class="nav-link" href="#">Layanan</a>
+            </li>
+            <li class="nav-item mx-3">
+              <a class="nav-link" href="#">Karir</a>
+            </li>
+            <li class="nav-item mx-3">
+              <a class="nav-link" href="#">Kontak Kami</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="page1"> 
+        <!-- page 1 -->
+        <!-- <img src="gambar/homepagewoman.jpg" alt=""> -->
+
+        <div class="judul">
+            <br>
+            <p style="font-family: 'Montserrat';font-size: 42px; ">Our Service <br> We Deliver</p>
+            <hr class="yellow-hr">
+        </div>
+    </div>
+
+    <div class="tombol">
+      <button class="tbl-chld">test</button>
+        <button class="tbl-chld">test</button>
+        <button class="tbl-chld">test</button>
+    </div>
+
+    <div class="page2">
+        <img src="../gambar/youngMan.png" class="youngMan">
+            <!-- <h3 class="tentangKami">Tentang Kami</h3> -->
+
+        <div class="tentangKami">
+            <h3>TENTANG KAMI</h3>
+            <HR>
+            <P>
+                Berdiri sejak pada tahun 2021, PT. Luar Biasa Teknologi 
+                merupakan perusahaan Business Process Outsourcing 
+                (BPO) berfokus pada aktifitas Collection yang telah 
+                di naungi AFPI dan OJK. 
+            </P>
+            <a href="../Tentang Kami/" class=""><button class="readMore"">Let's look at more</button></a>
+
+        </div>
+
+    </div>
+    
+    <div class="page3">
+        <br>
+        <h1 class="sertifikasi">SERTIFIKASI</h1>
+        <hr class="yellow-hr">
+        <img src="../gambar/AFPI.png" class="afpi">
+        
+
+        <p>Terdaftar sebagai Anggota <br> Asosiasi Fintech Pendanaan Bersama Indonesia <br> Nomor 032/REG/DCO/202</p>
+        <br>
+        
+        <hr>
+        <br>
+        <h1>Diawasi</h1>
+        <hr class="yellow-hr">
+        <img src="../gambar/OJK.png" class="ojk">
+        <br><br>
+    </div>
+
+    <div class="page4">
+        <h1>KLIEN KAMI</h1>
+        <hr class="yellow-hr">
+    </div>
+    
+    
+    <script 
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+
+    </script>
+
+</body>
+</html>
