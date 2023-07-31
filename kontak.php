dc0b40f07748dd64631b1b7c9b1010653f5591d4
rayypangestu <140422227+rayypangestu@users.noreply.github.com>
2023-07-31 14:31:54 +0800

    Kontak

--- kontak.php
+++ kontak.php
@@ -22,7 +22,7 @@
     </div>
 
     <div class="peta">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.9010580937593!2d110.33064801058373!3d-7.80029909218743!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a57b144f4e4f3%3A0xec833a1a245fd6a0!2sPT.%20Luar%20Biasa%20Teknologi!5e0!3m2!1sid!2sid!4v1690736130734!5m2!1sid!2sid" width="555" height="355" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.9010580937593!2d110.33064801058373!3d-7.80029909218743!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a57b144f4e4f3%3A0xec833a1a245fd6a0!2sPT.%20Luar%20Biasa%20Teknologi!5e0!3m2!1sid!2sid!4v1690736130734!5m2!1sid!2sid" width="650" height="355" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 
     <div class="sosmed">
