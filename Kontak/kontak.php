5d6b0318295222f5e3527a4de7bd9bfee9f20cc2
Roofiif Ananta Fathan <roofiif2000018419@webmail.uad.ac.id>
2023-08-04 00:03:11 +0700

    dikit lagi final, kurnag floating menu

--- Kontak/kontak.php
+++ Kontak/kontak.php
@@ -6,36 +6,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'> -->
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css" type="text/css">
     <title>Document</title>
 </head>
 <body>
     <hr>
     <div class="kontak-tentang">
-        <a href="#"> <p class="tentang">Tentang Kami</p> </a>
-        <a href="#"> <p class="kontak">Kontak Kami</p> </a>
+        <a href="../Tentang Kami"> <p class="tentang">Tentang Kami</p> </a>
+        <a href="../Kontak Kami"> <p class="kontak">Kontak Kami</p> </a>
     </div>
 
     <div class="alamat-perusahaan">
         <p class="perusahaan">PT Luar Biasa Teknologi</p>
-        <p class="alamat">Jl. Wates Jl. Sonopakis Lor No.185, <br> Onggobayan, Ngestiharjo, Kec. <br> Kasihan, Kabupaten Bantul,<br>  Daerah Istimewa Yogyakarta 55184</p>
+        <p class="alamat">Jl. Wates No.185, <br> Onggobayan, Ngestiharjo, Kec. <br> Kasihan, Kabupaten Bantul,<br>  Daerah Istimewa Yogyakarta 55184</p>
     </div>
 
-    <div class="peta">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.9010580937593!2d110.33064801058373!3d-7.80029909218743!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a57b144f4e4f3%3A0xec833a1a245fd6a0!2sPT.%20Luar%20Biasa%20Teknologi!5e0!3m2!1sid!2sid!4v1690736130734!5m2!1sid!2sid" width="650" height="355" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <div class="peta text-center align-content-center">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.9010580937593!2d110.33064801058373!3d-7.80029909218743!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a57b144f4e4f3%3A0xec833a1a245fd6a0!2sPT.%20Luar%20Biasa%20Teknologi!5e0!3m2!1sid!2sid!4v1690736130734!5m2!1sid!2sid" class="img-peta" width="650" height="355" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 
     <div class="sosmed">
-        <a href="#"> <img src="../gambar/instagram.png" alt="instagram" class="ikon"> </a>
-        <a href="#"> <img src="../gambar/linkedin.png" alt="linkedin" class="ikon"> </a>
-        <a href="#"> <img src="../gambar/facebook.png" alt="facebook" class="ikon"> </a>
+        <a href="#"> <img src="../Gambar/instagram.png" alt="instagram" class="ikon"> </a>
+        <a href="#"> <img src="../Gambar/linkedin.png" alt="linkedin" class="ikon"> </a>
+        <a href="#"> <img src="../Gambar/facebook.png" alt="facebook" class="ikon"> </a>
     </div>
 
     <hr>
 
     <div class="phone-mail">
-        <img src="../gambar/phone.png" alt="phone" class="ikon">0274-453053</img>
-        <img src="../gambar/mail.png" alt="mail" class="ikon">luarbiasatek@gmail.com</img>
+        <img src="../Gambar/phone.png" alt="phone" class="ikon">0274-453053</img>
+        <img src="../Gambar/mail.png" alt="mail" class="ikon">luarbiasatek@gmail.com</img>
     </div>
 
     <script 
